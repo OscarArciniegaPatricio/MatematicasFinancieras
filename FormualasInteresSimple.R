1c6972174f0249a03a8c7cfd947f469140bfa79d
OscarArciniegaPatricio <2032341x@umich.mx>
2024-09-19 12:40:59 -0600

    Carga Tarea V: 1.0
    Primera versión de la Tarea1

---
+++ FormualasInteresSimple.R
@@ -0,0 +1,10 @@
+#Realizamos el primer ejercicio para Valor Final (VF)
+# Autor: Oscar Arciniega Patricio
+# V 1.0: 19 de septiembre de 2024
+valorFinalSimple=function(VA,r,t){
+  xsalida=VA*(1+(r*t))
+  
+  return(xsalida)
+}
+
+#Despues que ya tengo mi formula, si quiero verificar de manera local es con la terminal de abajo|
