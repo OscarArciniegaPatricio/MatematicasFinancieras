1c7f4bc92ab25f7eb137db91726093e26bec8404
OscarArciniegaPatricio <2032341x@umich.mx>
2024-09-20 19:23:44 -0600

    Add files via upload
    Corrección de errores

---
+++ FormualasInteresSimple3.R
@@ -0,0 +1,28 @@
+#Realizamos el primer ejercicio para Valor Final (VF)
+# Autor: Oscar Arciniega Patricio
+# V 1.0: 19 de septiembre de 2024
+valorFinalSimple=function(VA,r,t){
+  xsalida=VA*(1+(r*t))
+  
+  return(xsalida)
+}
+
+valorActualSimple=function(VF,r,t){
+  xsalida2=VF/(1+(t*r))
+  
+  return(xsalida2)
+}
+
+TasaInteresSimple=function(VF,VA,t){
+  xsalida3=((VF/VA)-1)/t
+  
+  return(xsalida3)
+}
+TiempoSimple=function(VF,VA,r){
+  xsalida4=((VF/VA)-1)/r
+  
+  return(xsalida4)
+}
+
+
+#Despues que ya tengo mi formula, si quiero verificar de manera local es con la terminal de abajo|
